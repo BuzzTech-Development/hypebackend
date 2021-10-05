2ddfea2297fa04cb1c6092fd9c62a473b121303c
David Adams <dadams2212@gmail.com>
2021-10-05 09:31:43 -0400

    Create new Meeting model

--- hypeapi/admin.py
+++ hypeapi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Cohort, Profile, Assignment
+from .models import Cohort, Profile, Assignment, Meeting
 
 admin.site.register(Cohort)
 admin.site.register(Profile)
 admin.site.register(Assignment)
+admin.site.register(Meeting)
--- hypeapi/models.py
+++ hypeapi/models.py
@@ -37,3 +37,15 @@ class Assignment(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Meeting(models.Model):
+    name = models.CharField(max_length=50)
+    cohort = models.ForeignKey(Cohort, related_name='cohort', on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    link = models.TextField()
+
+    def __str__(self):
+        return self.name
