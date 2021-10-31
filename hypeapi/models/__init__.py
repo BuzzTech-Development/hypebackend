eec7dc796817680636bf16ca79f0be8b77315697
Jackson Wydra <jacksonwydra@gmail.com>
2021-10-31 17:56:18 -0400

    Added submission model

--- hypeapi/models/__init__.py
+++ hypeapi/models/__init__.py
@@ -2,3 +2,4 @@ from .assignment import Assignment
 from .cohort import Cohort
 from .meeting import Meeting
 from .profile import Profile
+from .submission import Submission
---
+++ hypeapi/models/submission.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+from . import Assignment, Profile
+
+
+class Submission(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    comments = models.CharField(max_length=200, default="")
+    points = models.IntegerField(default=0)
+    student = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
