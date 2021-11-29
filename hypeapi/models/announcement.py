aafc22d481846961763657659714191659b44fda
Jackson Wydra <jacksonwydra@gmail.com>
2021-11-28 21:51:31 -0500

    Fixed submission uploads

--- hypeapi/models/announcement.py
+++ hypeapi/models/announcement.py
@@ -1,16 +1,17 @@
+from django.core.mail import send_mail
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
+
 from .cohort import Cohort
 from .profile import Profile
 
 
 class Announcement(models.Model):
-    subject = models.CharField(max_length=200)
-    text = models.TextField()
     cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    subject = models.CharField(max_length=200)
+    text = models.TextField()
 
     def __str__(self):
         return self.subject
@@ -21,4 +22,5 @@ def send_email_notification(sender, instance, created, **kwargs):
     if created:
         user_list = Profile.objects.filter(cohorts__in=[instance.cohort])
         email_list = [profile.user.email for profile in user_list]
-        send_mail(instance.subject, instance.text, 'mail@gethype.org', recipient_list=email_list, fail_silently=True)
+        send_mail(instance.subject, instance.text, 'mail@gethype.org',
+                  recipient_list=email_list, fail_silently=True)
